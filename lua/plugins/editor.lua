7cccdc8b4237e0c95fa6ea864355028a02cae21a
Himalian <Voltage15312@outlook.com>
2026-03-22 23:03:27 +0800

    chore: update flash settings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -140,7 +140,13 @@ return {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		---@type Flash.Config
-		opts = {},
+		opts = {
+			modes = {
+				char = {
+					search = { wrap = true },
+				},
+			},
+		},
 		-- stylua: ignore
 		keys = {
 			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
