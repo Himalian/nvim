ff81f98023255bcf319607f69d538d3bfc9395ca
Himalian <Voltage15312@outlook.com>
2026-04-08 13:32:55 +0800

    breaking(treesitter): rewrite treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,57 +1,46 @@
+local languages = {
+	"c",
+	"python",
+	"lua",
+	"vim",
+	"vimdoc",
+	"javascript",
+	"html",
+	"nu",
+	"nix",
+}
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		-- build = function()
-		-- 	require("nvim-treesitter.install").update({ with_sync = true })()
-		-- end,
-		tags = "0.8.5.2",
+		lazy = true,
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html", "nu" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-			-- setup treesitter_selection
-			vim.keymap.set("n", "<S-CR>", "") -- disable default <S-CR> mapping
-			vim.keymap.set("n", "<C-CR>", "<CR>")
-			-- require("incr").setup(opts)
-			vim.keymap.set(
-				{ "n", "x" },
-				"<CR>",
-				require("utils").ts_selection.increment_selection,
-				{ desc = "Increment selection" }
-			)
-			vim.keymap.set(
-				"x",
-				"<S-CR>",
-				require("utils").ts_selection.decrement_selection,
-				{ desc = "Decrement selection" }
-			)
-			--- @class ParserConfig
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		config = function()
+			require("nvim-treesitter").install(languages)
 
-			parser_config.dae = {
-				install_info = {
-					url = "~/src/projects/tree-sitter-dae", -- local path or git repo
-					files = { "src/parser.c" },
-				},
-				filetype = "dae",
-			}
+			require("nvim-treesitter").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+
+			-- Enable highlighting per filetype (no longer automatic)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = languages,
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+
+			-- Native incremental selection (replaces plugin module)
+			vim.keymap.set("n", "<CR>", function()
+				vim.cmd("normal! v")
+				vim.treesitter.incremental_selection()
+			end)
+			vim.keymap.set("x", "<CR>", function()
+				vim.treesitter.incremental_selection()
+			end)
+			vim.keymap.set("x", "<S-CR>", function()
+				vim.treesitter.incremental_selection(-1)
+			end)
 		end,
-	},
-
-	{
-		"daliusd/incr.nvim",
-		event = "VeryLazy",
-		opts = {
-			incr_key = "<CR>", -- increment selection key
-			decr_key = "<S-CR>", -- decrement selection key
-		},
-		cond = false,
-		config = function(_, opts) end,
 	},
 }
