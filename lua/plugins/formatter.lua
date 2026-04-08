d4b913a87f41b97a8767347ec260e9ba58297a59
Himalian <Voltage15312@outlook.com>
2026-04-08 13:32:12 +0800

    formatter: udpate formatter settings

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,13 +3,13 @@ return {
 	event = "VeryLazy",
 	opts = {
 		formatters_by_ft = {
-			lua = { "stylua" },
+			lua = { "stylua", lsp_format = "fallback" },
 			-- Conform will run multiple formatters sequentially
-			python = { "ruff", "black", "isort" },
+			python = { "ruff", "black", "isort", stop_after_first = true },
 			-- You can customize some of the format options for the filetype (:help conform.format)
 			rust = { "rustfmt", lsp_format = "fallback" },
 			-- Conform will run the first available formatter
-			javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "biome", "prettier", stop_after_first = true, lsp_format = "fallback" },
 			cpp = { "clang-format" },
 			nix = { "nixfmt" },
 			go = { "gofmt" },
